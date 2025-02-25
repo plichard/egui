58f050f9865f53cc767485bc5b46350c357f71e2
Peter Lichard <peter.lichard@gmail.com>
2025-02-25 16:46:38 +0100

    Wip

--- crates/egui-winit/src/lib.rs
+++ crates/egui-winit/src/lib.rs
@@ -273,6 +273,8 @@ impl State {
         }
 
         use winit::event::WindowEvent;
+
+        log::error!("Window event: {:?}", event);
         match event {
             WindowEvent::ScaleFactorChanged { scale_factor, .. } => {
                 let native_pixels_per_point = *scale_factor as f32;
@@ -346,6 +348,7 @@ impl State {
                 //
                 // We use input_method_editor_started to manually insert CompositionStart
                 // between Commits.
+                log::error!("IME event received: {:?}", ime);
                 match ime {
                     winit::event::Ime::Enabled => {
                         if cfg!(target_os = "linux") {
@@ -383,6 +386,11 @@ impl State {
                 is_synthetic,
                 ..
             } => {
+                log::error!(
+                    "KeyboardInput event received: {:?}, is_synthetic: {}",
+                    event,
+                    is_synthetic
+                );
                 // Winit generates fake "synthetic" KeyboardInput events when the focus
                 // is changed to the window, or away from it. Synthetic key presses
                 // represent no real key presses and should be ignored.
@@ -1864,6 +1872,7 @@ pub fn short_window_event_description(event: &winit::event::WindowEvent) -> &'st
         WindowEvent::HoveredFileCancelled { .. } => "WindowEvent::HoveredFileCancelled",
         WindowEvent::Focused { .. } => "WindowEvent::Focused",
         WindowEvent::KeyboardInput { .. } => "WindowEvent::KeyboardInput",
+        WindowEvent::TextInput { .. } => "WindowEvent::TextInput",
         WindowEvent::ModifiersChanged { .. } => "WindowEvent::ModifiersChanged",
         WindowEvent::Ime { .. } => "WindowEvent::Ime",
         WindowEvent::CursorMoved { .. } => "WindowEvent::CursorMoved",
